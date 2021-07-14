c354471007bc8491f7a2e737fd40910eff1b0fe8
Thiago Silva Marano <thiago.silva-marano@klarna.com>
2021-07-15 00:28:11 +0200

    Fix style

--- src/GuitarArm.js
+++ src/GuitarArm.js
@@ -4,7 +4,7 @@ import GuitarString from './GuitarString';
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(${(props) => props.columnCount}, 1fr);};
+  grid-template-columns: repeat(${(props) => props.columnCount}, 1fr);
   justify-items: center;
   place-items: center;
   grid-gap: 0px;
