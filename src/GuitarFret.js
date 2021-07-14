b5499ea59d875df9e097daadd92dd523c9f737d8
Thiago Silva Marano <thiago.silva-marano@klarna.com>
2021-07-15 00:29:13 +0200

    Simplify conditional

--- src/GuitarFret.js
+++ src/GuitarFret.js
@@ -3,7 +3,7 @@ import getNoteName from './getNoteName';
 
 export default function GuitarFret({ stringNoteIndex, fretIndex }) {
   return (
-    <div className={fretIndex === 0 ? 'open-string' : ''}>
+    <div className={fretIndex === 0 && 'open-string'}>
       {getNoteName(stringNoteIndex + fretIndex)}
     </div>
   );
