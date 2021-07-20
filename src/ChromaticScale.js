7327874ccc116cb872155f60c346d7baebbc3099
Thiago Silva Marano <thiago.silva-marano@klarna.com>
2021-07-20 22:08:06 +0200

    Show grid for chromatic scale

--- src/ChromaticScale.js
+++ src/ChromaticScale.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 import HighligthableNote from './HighlitableNote';
 import { noteIndexes } from './notes';
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${(props) => props.columnCount}, 1fr);
+  justify-items: center;
+  place-items: center;
+  grid-gap: 0px;
+
+  div {
+    border: 1px solid black;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
 export default function ChromaticScale() {
-  return noteIndexes.map((noteIndex) => <HighligthableNote key={noteIndex} index={noteIndex} />);
+  return (
+    <Grid columnCount={12}>
+      {noteIndexes.map((noteIndex) => <HighligthableNote key={noteIndex} index={noteIndex} />)}
+    </Grid>
+  );
 }
