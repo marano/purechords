5302b3e8066ae80f2c93f9c7dddfb33f75d36fb4
marano <thiagomarano@gmail.com>
2021-07-30 00:03:32 +0200

    Fix imports

--- src/utils/areNumberArraysEquals.tsx
+++ src/utils/areNumberArraysEquals.tsx
@@ -1,5 +1,5 @@
-import { getEq } from 'fp-ts/lib/Array';
-import { Eq } from 'fp-ts/lib/number';
+import { getEq } from 'fp-ts/Array';
+import { Eq } from 'fp-ts/number';
 
 export default function areNumberArraysEquals(arrayA: number[], arrayB: number[]) {
   return getEq(Eq).equals(arrayA, arrayB)
