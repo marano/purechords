84fe8766f528ffb5c30e9e72645087f52c2e112e
marano <thiagomarano@gmail.com>
2021-07-30 18:09:07 +0200

    Make fret type more descriptive

--- src/types.tsx
+++ src/types.tsx
@@ -1,3 +1,6 @@
 export type Note = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 
-export type Fret = [number, number]
+export type StringIndex = number
+export type FretIndex = number
+
+export type Fret = [StringIndex, FretIndex]
