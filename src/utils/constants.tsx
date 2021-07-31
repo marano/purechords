9904ac4c3ae706278492148fde52e50d282e1d09
marano <thiagomarano@gmail.com>
2021-07-31 12:34:03 +0200

    Add minor scales

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -17,10 +17,37 @@ export const noteColors = [
 ];
 
 export const scaleIntervals = {
-  major: [
+  major: [ // Ionian mode
     Interval.U,
     Interval.M2,
     Interval.M3,
+    Interval.P4,
+    Interval.P5,
+    Interval.M6,
+    Interval.M7
+  ],
+  naturalMinor: [ // Aeolian mode
+    Interval.U,
+    Interval.M2,
+    Interval.m3,
+    Interval.P4,
+    Interval.P5,
+    Interval.m6,
+    Interval.m7
+  ],
+  harmonicMinor: [ // Aeolian #7 scale
+    Interval.U,
+    Interval.M2,
+    Interval.m3,
+    Interval.P4,
+    Interval.P5,
+    Interval.m6,
+    Interval.M7
+  ],
+  melodicMinor: [
+    Interval.U,
+    Interval.M2,
+    Interval.m3,
     Interval.P4,
     Interval.P5,
     Interval.M6,
