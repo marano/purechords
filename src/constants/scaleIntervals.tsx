8f36d63453e10ba71c58240d8d18b604073cdd19
marano <thiagomarano@gmail.com>
2021-08-07 17:50:20 +0200

    Add pentatonic scales

--- src/constants/scaleIntervals.tsx
+++ src/constants/scaleIntervals.tsx
@@ -19,4 +19,6 @@ export default {
   'Natural Minor': [U, M2, m3, P4, P5, m6, m7, Oct],
   'Harmonic Minor': [U, M2, m3, P4, P5, m6, M7, Oct],
   'Melodic Minor': [U, M2, m3, P4, P5, M6, M7, Oct],
+  'Major Pentatonic': [U, M2, M3, P5, M6, Oct],
+  'Minor Pentatonic': [U, m3, P4, P5, m7, Oct],
 }
