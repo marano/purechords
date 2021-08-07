0cf7d9097f6c794acaa8d10a6da1b0985017dbcc
marano <thiagomarano@gmail.com>
2021-08-07 09:44:14 +0200

    Decrease size of root circle a bit

--- src/components/GuitarArm/RootCircle.tsx
+++ src/components/GuitarArm/RootCircle.tsx
@@ -8,7 +8,7 @@ export default styled.span`
   background-color: ${(props: CircleProps) => props.isHighlighted ? 'black' : 'transparent'};
   border-radius: 100%;
   color: ${(props: CircleProps) => props.isHighlighted ? 'white' : 'black'};
-  padding-left: 5px;
-  padding-right: 5px;
+  padding-left: 4px;
+  padding-right: 4px;
 `
 
