5e709582b06d6e7285cc7b7b2325ea86d0c3902f
marano <thiagomarano@gmail.com>
2022-05-08 14:05:51 +0200

    Add test for getVoicedChord

---
+++ src/utils/getVoicedChord.test.tsx
@@ -0,0 +1,17 @@
+import { Note } from '../types'
+import getVoicedChord from './getVoicedChord'
+import chordVoicings from '../constants/chordVoicings'
+
+describe('getVoicedChord', () => {
+  test('Cmaj 1/3/5', () =>
+    expect(
+      getVoicedChord([Note.C, Note.E, Note.G], chordVoicings.Triad[0])
+    ).toEqual([Note.C, Note.E, Note.G])
+  )
+
+  test('Cmaj 3/5/1', () =>
+    expect(
+      getVoicedChord([Note.C, Note.E, Note.G], chordVoicings.Triad[1])
+    ).toEqual([Note.E, Note.G, Note.C])
+  )
+})
